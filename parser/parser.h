5990471b77b732fcd1d1678eb2c7e275892835f3
mk3058 <kmk3058@icloud.com>
2023-08-11 18:08:49 +0900

    feat(parser): add .rt parser

--- parser/parser.h
+++ parser/parser.h
@@ -6,21 +6,24 @@
 /*   By: minkyuki <minkyuki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 13:05:53 by minkyuki          #+#    #+#             */
-/*   Updated: 2023/08/11 14:16:58 by minkyuki         ###   ########.fr       */
+/*   Updated: 2023/08/11 17:24:46 by minkyuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
 
-# include "object.h"
+# include "../object/object.h"
 # include "../libft/libft.h"
-# include "../get_next_line/get_next_line.h"
+# include "./get_next_line/get_next_line.h"
 # include "../error_handler/error_handler.h"
 # include <stdio.h>
+# include <string.h>
 # include <fcntl.h>
 # include <errno.h>
 
-char	**rt_split(char *str, char *charset);
-void	destroy_token(char **token);
+char		**rt_split(char *str, char *charset);
+void		destroy_token(char **token);
+t_element	parse_rt(char *target);
+
 #endif
