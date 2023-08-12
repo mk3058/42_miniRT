03397429a1b67fe7866c915ff78e089d016a1d4b
Jongmin Im <jonim@c2r10s5.42seoul.kr>
2023-08-12 17:56:47 +0900

    main

--- main.c
+++ main.c
@@ -2,28 +2,6 @@
 #include "object.h"
 #include "parser.h"
 
-void	minirt_mlx_init(t_data *mlx)
-{
-	mlx->mlx = mlx_init();
-	mlx->mlx_win = mlx_new_window(mlx->mlx, CANVAS_HEI, CANVAS_WID, "miniRT");
-	mlx->img = mlx_new_image(mlx->mlx, CANVAS_HEI, CANVAS_WID);
-	mlx->addr = mlx_get_data_addr(mlx->img, &mlx->bbp, &mlx->len, &mlx->endian);
-}
-
-void	my_mlx_pixel_put(t_data *mlx, int x, int y, t_color color)
-{
-	char	*dst;
-
-	dst = mlx->addr + (y * mlx->len + x * (mlx->bbp / 8));
-}
-
-int	exit_win(t_data *mlx)
-{
-	mlx_destroy_image(mlx->mlx, mlx->img);
-	mlx_destroy_window(mlx->mlx, mlx->mlx_win);
-	exit(0);
-}
-
 int	main(int argc, char **argv)
 {
 	t_data		mlx;
