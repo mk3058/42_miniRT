8440eb7ad4186fed1d1d3eeafa17d953009a91cc
mk3058 <kmk3058@icloud.com>
2023-08-15 08:28:07 +0900

    fix(ray): fix ray_tracing function

--- trace/trace.c
+++ trace/trace.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   trace.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minkyuki <minkyuki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: minkyu <minkyu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 12:49:03 by minkyuki          #+#    #+#             */
-/*   Updated: 2023/08/14 13:42:45 by minkyuki         ###   ########.fr       */
+/*   Updated: 2023/08/15 08:27:22 by minkyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_ray	ray_tracing(const t_element element, int h, int v)
 	ray.orig = element.camera.origin;
 	ray.dir = vunit(vsub(vadd(vadd(element.camera.top_left, \
 					vmul_(element.camera.horizontal, h)),
-					vmul_(element.camera.vertical, v)), \
+					vmul_(element.camera.vertical, -v)), \
 					element.camera.origin));
 	return (ray);
 }
