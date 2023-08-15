9dbf9f8816b6a630451e3182a1404f555c6a4917
mk3058 <kmk3058@icloud.com>
2023-08-15 10:52:26 +0900

    fix(ray): fix ray direction calculation function

--- trace/trace.c
+++ trace/trace.c
@@ -6,7 +6,7 @@
 /*   By: minkyu <minkyu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 12:49:03 by minkyuki          #+#    #+#             */
-/*   Updated: 2023/08/15 08:27:22 by minkyu           ###   ########.fr       */
+/*   Updated: 2023/08/15 10:51:48 by minkyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,8 @@ t_ray	ray_tracing(const t_element element, int h, int v)
 	t_ray	ray;
 
 	ray.orig = element.camera.origin;
-	ray.dir = vunit(vsub(vadd(vadd(element.camera.top_left, \
-					vmul_(element.camera.horizontal, h)),
-					vmul_(element.camera.vertical, -v)), \
-					element.camera.origin));
+	ray.dir = vunit(vadd(vadd(element.camera.top_left, \
+				vmul_(element.camera.horizontal, h)),
+				vmul_(element.camera.vertical, -v)));
 	return (ray);
 }
