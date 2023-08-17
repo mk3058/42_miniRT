5a4d1fe117c5a3b2e531e8b7e3dd97a5b343b401
mk3058 <kmk3058@icloud.com>
2023-08-18 00:32:57 +0900

    fix(viewport): fix the camera's position to be considered in creating a top vector

--- object/viewport_util.c
+++ object/viewport_util.c
@@ -6,7 +6,7 @@
 /*   By: minkyu <minkyu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 12:12:29 by minkyuki          #+#    #+#             */
-/*   Updated: 2023/08/17 20:15:01 by minkyu           ###   ########.fr       */
+/*   Updated: 2023/08/18 00:31:50 by minkyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,32 @@
 #include "minirt.h"
 #include <limits.h>
 
-static t_vec	get_approx_top(t_camera *camera, const t_vec dir[2])
+static t_vec	approx_top_by_position(t_camera *camera)
+{
+	t_point	point;
+	t_vec	top;
+
+	point = camera->origin;
+	if (point.x <= point.z && point.x <= -point.z)
+		top = (t_vec){-1, 0, 0};
+	else if (point.x <= point.z && point.x >= -point.z)
+		top = (t_vec){0, 0, 1};
+	else if (point.x >= point.z && point.x >= -point.z)
+		top = (t_vec){1, 0, 0};
+	else if (point.x >= point.z && point.x <= -point.z)
+		top = (t_vec){0, 0, -1};
+	if (camera->d_vec.y > EPSILON)
+		vmul_(top, -1);
+	return (top);
+}
+
+static t_vec	get_approx_top(t_camera *camera)
 {
 	int			i;
 	t_vec		top_dir;
 	double		top_ang;
 	double		cur_ang;
+	const t_vec	dir[2] = {{0, 1, 0}, {0, -1, 0}};
 
 	i = 0;
 	top_ang = INT_MAX;
@@ -28,7 +48,7 @@ static t_vec	get_approx_top(t_camera *camera, const t_vec dir[2])
 		cur_ang = fabs(acos(vdot(camera->d_vec, dir[i]) \
 			/ (vlen(camera->d_vec) * vlen(dir[i]))));
 		if (cur_ang < EPSILON)
-			return ((t_vec){-camera->d_vec.y, 0, 0});
+			return (approx_top_by_position(camera));
 		if (fabs(cur_ang - M_PI_2) < EPSILON)
 			return (dir[i]);
 		if (fabs(cur_ang - M_PI_2) < fabs(top_ang - M_PI_2))
@@ -59,7 +79,7 @@ void	set_viewport(t_camera *camera)
 	ratio = (double)CANVAS_WID / (double)CANVAS_HEI;
 	camera->viewport_height = 2.0;
 	camera->viewport_width = camera->viewport_height * ratio;
-	approx_top = get_approx_top(camera, (t_vec[]){{0, 1, 0}, {0, -1, 0}});
+	approx_top = get_approx_top(camera);
 	camera->horizontal = vunit(vcrs(camera->d_vec, approx_top));
 	camera->vertical = vunit(vcrs(camera->horizontal, camera->d_vec));
 	camera->focal_length = get_focal_length(camera);
