60f4c01acbc409fdcd744e637471cff190ece9a3
mk3058 <93522877+mk3058@users.noreply.github.com>
2023-08-20 15:15:46 +0900

    mlx key hook handler 추가 및 기타 오류 수정 (#28)
    * feat(mlx): add 'esc' key hook handler
    
    * fix(norminette): fix convention error
    
    * feat(parser): add camera value check
    
    * feat(viewport): add exception handling for fov

--- main.c
+++ main.c
@@ -1,19 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minkyuki <minkyuki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/18 15:19:18 by minkyuki          #+#    #+#             */
+/*   Updated: 2023/08/18 15:25:54 by minkyuki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minirt.h"
 #include "object.h"
 #include "parser.h"
 #include "trace.h"
 
-int	main(int argc, char **argv)
+void	put_color(t_element element, t_data mlx)
 {
-	t_data		mlx;
-	t_element	element;
-	int			height;
-	int			width;
+	int	width;
+	int	height;
 
-	if (argc != 2)
-		print_exit("%s\n", "Invalid number of arguments");
-	element = parse_rt(argv[1]);
-	minirt_mlx_init(&mlx);
 	height = 0;
 	while (height < CANVAS_HEI)
 	{
@@ -28,7 +34,22 @@ int	main(int argc, char **argv)
 		}
 		height++;
 	}
+}
+
+int	main(int argc, char **argv)
+{
+	t_data		mlx;
+	t_element	element;
+	int			height;
+	int			width;
+
+	if (argc != 2)
+		print_exit("%s\n", "Invalid number of arguments");
+	element = parse_rt(argv[1]);
+	minirt_mlx_init(&mlx);
+	put_color(element, mlx);
 	mlx_put_image_to_window(mlx.mlx, mlx.mlx_win, mlx.img, 0, 0);
-	mlx_hook(mlx.mlx_win, 17, 0, &exit_win, &mlx);
+	mlx_hook(mlx.mlx_win, KEYRELEASE, KEYPRESS_MASK, key_hook_handler, &mlx);
+	mlx_hook(mlx.mlx_win, DESTROYNOTIFY, NO_MASK, exit_win, &mlx);
 	mlx_loop(mlx.mlx);
 }
