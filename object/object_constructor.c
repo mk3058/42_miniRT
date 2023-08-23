a79568df7f16040540b14cb9a12d7135674fbd71
mk3058 <93522877+mk3058@users.noreply.github.com>
2023-08-23 15:57:20 +0900

    fix(arguments): add argument value validation (#34)

--- object/object_constructor.c
+++ object/object_constructor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   object_constructor.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minkyu <minkyu@student.42.fr>              +#+  +:+       +#+        */
+/*   By: minkyuki <minkyuki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 15:39:30 by minkyuki          #+#    #+#             */
-/*   Updated: 2023/08/17 10:11:46 by minkyu           ###   ########.fr       */
+/*   Updated: 2023/08/23 15:47:00 by minkyuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,11 @@ t_ambient	ambient(char **token, t_element *element)
 		i++;
 	if (i != 5)
 		print_exit("'%s': Invalid number of arguments.\n", token[0]);
-	a.brightness = ft_atof(token[1]);
+	a.brightness = ft_atof(token[1], 0, 1);
 	if (a.brightness > 1 + EPSILON)
 		a.brightness /= 255;
-	a.color = vdiv(color(ft_atof(token[2]), ft_atof(token[3]), \
-					ft_atof(token[4])), 255);
+	a.color = vdiv(color(ft_atof(token[2], 0, 255), \
+		ft_atof(token[3], 0, 255), ft_atof(token[4], 0, 255)), 255);
 	element->ambient = a;
 	return (a);
 }
@@ -47,12 +47,13 @@ t_light	light(char **token, t_element *element)
 		print_exit("'%s': Invalid number of arguments.\n", token[0]);
 	new = malloc(sizeof(t_light));
 	new->origin = \
-	point(ft_atof(token[1]), ft_atof(token[2]), ft_atof(token[3]));
-	new->brightness = ft_atof(token[4]);
+	point(ft_atof(token[1], -1, -1), ft_atof(token[2], -1, -1), \
+			ft_atof(token[3], -1, -1));
+	new->brightness = ft_atof(token[4], 0, 1);
 	if (new->brightness > 1 + EPSILON)
 		new->brightness /= 255;
-	new->color = vdiv(color(ft_atof(token[5]), ft_atof(token[6]), \
-						ft_atof(token[7])), 255);
+	new->color = vdiv(color(ft_atof(token[5], 0, 255), \
+		ft_atof(token[6], 0, 255), ft_atof(token[7], 0, 255)), 255);
 	o = calloc(1, sizeof(t_object));
 	o->obj = new;
 	o->type = LIGHT;
@@ -73,10 +74,11 @@ t_camera	camera(char **token, t_element *element)
 		i++;
 	if (i != 8)
 		print_exit("'%s': Invalid number of arguments.\n", token[0]);
-	c.origin = point(ft_atof(token[1]), ft_atof(token[2]), ft_atof(token[3]));
-	c.d_vec = vunit(vec(ft_atof(token[4]), ft_atof(token[5]), \
-					ft_atof(token[6])));
-	c.fov = ft_atof(token[7]);
+	c.origin = point(ft_atof(token[1], -1, -1), ft_atof(token[2], -1, -1), \
+						ft_atof(token[3], -1, -1));
+	c.d_vec = vunit(vec(ft_atof(token[4], -1, -1), ft_atof(token[5], -1, -1), \
+					ft_atof(token[6], -1, -1)));
+	c.fov = ft_atof(token[7], 0, 180);
 	set_viewport(&c);
 	element->camera = c;
 	return (c);
@@ -94,10 +96,11 @@ t_sphere	sphere(char **token, t_element *element)
 	if (i != 8)
 		print_exit("'%s': Invalid number of arguments.\n", token[0]);
 	c = malloc(sizeof(t_sphere));
-	c->origin = point(ft_atof(token[1]), ft_atof(token[2]), ft_atof(token[3]));
-	c->radius = ft_atof(token[4]) / 2.0;
-	c->color = vdiv(color(ft_atof(token[5]), ft_atof(token[6]), \
-							ft_atof(token[7])), 255);
+	c->origin = point(ft_atof(token[1], -1, -1), ft_atof(token[2], -1, -1), \
+						ft_atof(token[3], -1, -1));
+	c->radius = ft_atof(token[4], 0, -1) / 2.0;
+	c->color = vdiv(color(ft_atof(token[5], 0, 255), ft_atof(token[6], 0, 255), \
+							ft_atof(token[7], 0, 255)), 255);
 	o = ft_calloc(1, sizeof(t_object));
 	o->obj = c;
 	o->type = SPHERE;
@@ -117,11 +120,12 @@ t_plane	plane(char **token, t_element *element)
 	if (i != 10)
 		print_exit("'%s': Invalid number of arguments.\n", token[0]);
 	c = malloc(sizeof(t_plane));
-	c->origin = point(ft_atof(token[1]), ft_atof(token[2]), ft_atof(token[3]));
-	c->n_vec = vunit(vec(ft_atof(token[4]), ft_atof(token[5]), \
-					ft_atof(token[6])));
-	c->color = vdiv(color(ft_atof(token[7]), ft_atof(token[8]), \
-							ft_atof(token[9])), 255);
+	c->origin = point(ft_atof(token[1], -1, -1), ft_atof(token[2], -1, -1), \
+						ft_atof(token[3], -1, -1));
+	c->n_vec = vunit(vec(ft_atof(token[4], -1, -1), ft_atof(token[5], -1, -1), \
+					ft_atof(token[6], -1, -1)));
+	c->color = vdiv(color(ft_atof(token[7], 0, 255), ft_atof(token[8], 0, 255), \
+							ft_atof(token[9], 0, 255)), 255);
 	o = ft_calloc(1, sizeof(t_object));
 	o->obj = c;
 	o->type = PLANE;
